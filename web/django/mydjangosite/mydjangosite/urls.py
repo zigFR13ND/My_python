c702e6ba64ffaa4f539957247e9dcaffb401362f
Ilnur Ziganshin <zig.i@bk.ru>
2025-03-02 12:38:17 +0300

    Django practic_ 1

--- web/django/mydjangosite/mydjangosite/urls.py
+++ web/django/mydjangosite/mydjangosite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for mydjangosite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,14 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include  # include позволяет подключать URL-приложений
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('webapp.urls')),  # перенаправляем корневой URL в urls приложения webapp
 ]
-
-
-
---
+++ web/django/mydjangosite/webapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+def home(request):
+    """
+    Простое представление, возвращающее HTML с приветствием.
+    """
+    return HttpResponse("<h1>Добро пожаловать в Django Demo!</h1><p>Это учебное приложение на Django.</p>")
+
+
+def api_info(request):
+        """
+    Возвращает информацию о приложении в формате JSON.
+    """
