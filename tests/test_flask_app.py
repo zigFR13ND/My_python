6d4c67ae4f4eafc05b40536932fa9f29542869ee
Ilnur Ziganshin <zig.i@bk.ru>
2025-03-04 11:56:32 +0300

    test

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -2,7 +2,8 @@ import unittest
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from flask_app import app, db, User
+from db_app import app, db, User
+
 
 class FlaskAppTestCase(unittest.TestCase):
     def setUp(self):
