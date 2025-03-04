a1d52a83afdf9795a16a8b2d863f22351ac714fb
Ilnur Ziganshin <zig.i@bk.ru>
2025-03-04 11:58:02 +0300

    test

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'orm')))
 from db_app import app, db, User
 
 
