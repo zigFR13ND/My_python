6f06e675aa34383ccd8a522b6c622ce56f2b6056
Ilnur Ziganshin <zig.i@bk.ru>
2025-03-11 17:26:51 +0300

    работа api git env + secret

--- moduls_and_libraries/git_Bearer_Token.py
+++ moduls_and_libraries/git_Bearer_Token.py
@@ -5,8 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()  # Загружаем переменные из .env
 
 token = os.getenv("GITHUB_TOKEN")  # Получаем токен
-url = "https://api.github.com/user"
-headers = {"Authorization": f"Bearer {token}"}
 
-response = requests.get(url, headers=headers)
-print(response.json())
+if not token:  # Если нет локального .env, пробуем GitHub Secret
+    token = os.getenv("GITHUB_ACTIONS_SECRET")
+
+if not token:
+    raise ValueError("❌ Токен не найден! Проверь .env или GitHub Secrets.")
+
+print("✅ Токен загружен!")
